d227c15e88856c9108b03ffc95279a176155cdaa
Gelatinya <gelatinamp@gmail.com>
2026-07-20 20:53:09 -0500

    Update PF_v12.java

--- src/pf_v1/pkg2/PF_v12.java
+++ src/pf_v1/pkg2/PF_v12.java
@@ -24,7 +24,7 @@ public class PF_v12 {
             do {
                 try {
                     opcion = sc.nextInt();
-                    if (opcion < 1 && opcion > 5){
+                    if (opcion < 1 || opcion > 5){
                         System.out.println("Opcion invalida. Numero fuera del rango");
                         System.out.print("Ingrese la opcion nuevamente (1 - 6):");
                     }
@@ -33,7 +33,7 @@ public class PF_v12 {
                     sc.nextLine();
                     System.out.print("Ingrese la opcion nuevamente (1 - 6):");
                 }
-            } while (opcion < 1 && opcion > 5);
+            } while (opcion < 1 || opcion > 5);
             
             switch (opcion) {
                 case 1:
@@ -76,45 +76,42 @@ public class PF_v12 {
                     sc.nextLine();
                     System.out.print("Ingrese nuevamente el codigo: ");
                 }
-            }while (codigo < 99 || codigo > 999);
+            } while (codigo < 99 && codigo > 999);
             sc.nextLine();
             
             System.out.println("Ingrese solo nombres completos: ");
-            //Validador del Nombre completo del estudiante 
             do {
-                NombreC= sc.nextLine();
-                if (NombreC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ]") == false){
+                NombreC = sc.nextLine();
+                if (NombreC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+") == false) {
                     System.out.println("Caracter incorrecto");
-                    System.out.println("Ingrese el nombre nuevamente:");
+                    System.out.println("Ingrese los nombre nuevamente:");
                 }
-            }while(NombreC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ]+") == false);
+            } while (NombreC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+") == false);
             sc.nextLine();
             
             System.out.println("Ingrese sus apellidos: ");
-            //Validador del apellido
             do {
-                ApellidoC= sc.nextLine();           
-                if (ApellidoC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ]") == false){
+                ApellidoC = sc.nextLine();           
+                if (ApellidoC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+") == false){
                     System.out.println("Caracter incorrecto");
-                    System.out.println("Ingrese el apellido nuevamente:");
+                    System.out.println("Ingrese los apellido nuevamente:");
                 }
-            }while(ApellidoC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ]") == false);
+            } while (ApellidoC.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+") == false);
             
             System.out.print("Ingrese edad del estudiante: ");
-            //Validador de la edad del estudiante
             do {
                 try {
-                edad=sc.nextInt();
-                if (edad < 16 || edad > 35){
-                    System.out.println("Edad incorrecta rango de edad -> (16 - 35)");
+                edad = sc.nextInt();
+                if (edad < 16 || edad > 50){
+                    System.out.println("Edad incorrecta rango de edad -> (16 - 50)");
                     System.out.print("Ingrese la edad de nuevo: ");
                 }
-                } catch(InputMismatchException e){
+                } catch (InputMismatchException e) {
                     System.out.println("Error: Solo se permiten ingresar numeros no otras variables");
                     sc.nextLine();
                     System.out.print("Ingrese nuevamente la edad: ");
                 }
-            }while (edad < 16 || edad > 35);
+            } while (edad < 16 || edad > 50);
             
             String sql = "INSERT INTO Estudiantes (Codigo, nombre, apellido, edad) VALUES (?, ?, ?, ?)";
             
@@ -125,7 +122,7 @@ public class PF_v12 {
                 Consult.setInt(4, edad);
                 Consult.executeUpdate();
                 System.out.println("Estudiante registrado correctamente!!!");
-            } catch (SQLException e){
+            } catch (SQLException e) {
                 System.out.println("Error al registrar: " + e.getMessage());
             }
             
@@ -134,35 +131,32 @@ public class PF_v12 {
                 System.out.println("1.  Registrar otro alumno");
                 System.out.println("2.  Regresar al menu");
                 System.out.println("---------------------------------------");
-                //Nuevo
-                //Aqui falto poner un validador para las opcioens 1 y 2 pq cuando le puse 3 o una letra me salio error
                 do {
                     try {
-                        opcion = sc.nextInt();
-                        if (opcion<1 || opcion>2){
-                            System.out.println("Opcion invalida; solo ingresar opciones (1-2)");
-                            System.out.print("Ingrese nuevamente la opcion: ");
+                        opcion= sc.nextInt();
+                        if (opcion < 1 || opcion > 2){
+                            System.out.println("Opcion invalida. Numero fuera del rango");
+                            System.out.print("Ingrese la opcion nuevamente (1 - 2):");
                         }
-                    }catch (InputMismatchException e){
-                        System.out.println("Las letras no estan permitidas como opciones");
+                    } catch (InputMismatchException e){
+                        System.out.println("Opcion invalida. No se adminten letras");
                         sc.nextLine();
-                        System.out.print("Selecciona nuevamente una opcion: ");
-                                                
+                        System.out.print("Ingrese la opcion nuevamente (1 - 2):");
                     }
-                }while (opcion<1||opcion>2);
-            } while (opcion != 1 && opcion !=2 );
+                } while (opcion < 1 || opcion > 2);
+            } while (opcion != 1 && opcion != 2 );
         } while (opcion != 2);    
     }
     
     private static void lista() {
         String sql = "SELECT Codigo, nombre, apellido, edad FROM Estudiantes";
         
-        try (Connection Conext = Coneccion_Bd.conectar(); Statement Paloma = Conext.createStatement()){
+        try (Connection Conext = Coneccion_Bd.conectar(); Statement Paloma = Conext.createStatement()) {
             ResultSet Envio = Paloma.executeQuery(sql);
             boolean Entregado = false;
             System.out.println("--------------------------[LISTA]------------------------");
             System.out.println("[CODIGO]     [EDAD]                [ALUMNO]              ");
-            while (Envio.next()){
+            while (Envio.next()) {
                 Entregado = true;
                 int codigo = Envio.getInt("codigo");
                 String nombre = Envio.getString("nombre");
@@ -174,40 +168,38 @@ public class PF_v12 {
             if (Entregado == false) {
                 System.out.println("No hay alumnos registrados");
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error al listar: " + e.getMessage());
         }
     }
     
     private static void buscar() {
-        int codBuscar=0, opcion = 0;
+        int codBuscar = 0, opcion = 0;
         boolean encontrado;
         
         do {
             System.out.println("Ingrese el codigo del alumno que quiere buscar");
-            //Nuevo
-            //Validador para bsucar codigo
-            do{
+            do {
                 try {
                     codBuscar = sc.nextInt();
                     if (codBuscar < 99 || codBuscar > 999){
-                        System.out.println("Codigo de estudiante fuera de rango");
+                        System.out.println("Codigo de estudiante no existe");
                         System.out.print("Ingrese nuevamente el codigo a buscar: ");
                     }
-                } catch(InputMismatchException e){
-                    System.out.println("Error: Solo se permiten ingresar numeros no otras variables");
+                } catch (InputMismatchException e) {
+                    System.out.println("Opcion invalida. No se adminten letras");
                     sc.nextLine();
                     System.out.print("Ingrese nuevamente el codigo: ");
                 }
-            }while (codBuscar < 99 || codBuscar > 999);
+            } while (codBuscar < 99 || codBuscar > 999);
             
             String sql = "SELECT Codigo, nombre, apellido, edad FROM Estudiantes  WHERE Codigo=?";
             encontrado = false;
             
-            try(Connection Conext = Coneccion_Bd.conectar(); PreparedStatement Consult = Conext.prepareStatement(sql)){
+            try (Connection Conext = Coneccion_Bd.conectar(); PreparedStatement Consult = Conext.prepareStatement(sql)) {
                 Consult.setInt(1, codBuscar);
                 ResultSet Envio = Consult.executeQuery();
-                if (Envio.next()){
+                if (Envio.next()) {
                     encontrado = true;
                     System.out.println("Alumno encontrado!");
                     System.out.println("---------------------------------------------------------");
@@ -215,12 +207,12 @@ public class PF_v12 {
                     System.out.println(" " + Envio.getInt("Codigo") + "          " + Envio.getInt("edad") + "          " + Envio.getString("nombre") + " " + Envio.getString("apellido"));
                     System.out.println("---------------------------------------------------------");        
                 }
-            }catch (SQLException Error101){
+            }catch (SQLException Error101) {
                 System.out.println("Error de busqueda: "+Error101.getMessage());
             }
             
             if (encontrado == false) {
-                System.out.println("El alumno con el codigo " + codBuscar + " no se encuentra en el registro");
+                System.out.println("El codigo " + codBuscar + " no se encuentra en el registro");
             }
                     
             do {
@@ -228,25 +220,21 @@ public class PF_v12 {
                 System.out.println("1. Buscar otro alumno");
                 System.out.println("2. Regresar al menu");
                 System.out.println("---------------------------------------");
-                //Nuevo
-                //Aqui falto poner un validador para las opcioens 1 y 2 pq cuando le puse 3 o una letra me salio error
                 do {
                     try {
-                        opcion = sc.nextInt();
-                        if (opcion<1 || opcion>2){
-                            System.out.println("Opcion invalida; solo ingresar opciones (1-2)");
-                            System.out.print("Ingrese nuevamente la opcion: ");
+                        opcion= sc.nextInt();
+                        if (opcion < 1 || opcion > 2){
+                            System.out.println("Opcion invalida. Numero fuera del rango");
+                            System.out.print("Ingrese la opcion nuevamente (1 - 2):");
                         }
-                    }catch (InputMismatchException e){
-                        System.out.println("Las letras no estan permitidas como opciones");
+                    } catch (InputMismatchException e){
+                        System.out.println("Opcion invalida. No se adminten letras");
                         sc.nextLine();
-                        System.out.print("Selecciona nuevamente una opcion: ");
-                                                
+                        System.out.print("Ingrese la opcion nuevamente (1 - 2):");
                     }
-                }while (opcion<1||opcion>2);
-            } while (opcion != 1 && opcion !=2 );
+                } while (opcion < 1 || opcion > 2);
+            } while (opcion != 1 && opcion != 2 );
         } while (opcion != 2);    
     }
-
 
 }
